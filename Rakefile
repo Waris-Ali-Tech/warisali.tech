4a761268be866f710056ab84d072779418efe137
Waris Ali <m.warisali2@gmail.com>
2020-10-06 20:53:04 +0500

    bugfix: extract comment from api response

--- Rakefile
+++ Rakefile
@@ -2,7 +2,7 @@ require "yaml"
 require "active_support/core_ext/hash/deep_merge"
 
 task :server do
-  system('bundle exec jekyll serve --config _config.yml,_staging.config.yml,_develop.config.yml')
+  system('RUBYOPT="-W0" bundle exec jekyll serve --config _config.yml,_staging.config.yml,_develop.config.yml')
 end
 
 
--- assets/js/comments.js
+++ assets/js/comments.js
@@ -33,7 +33,7 @@ function handle_api_response(el, data, xhr) {
     if (xhr.readyState === 4) {
         hide_loading(el)
         if (xhr.status === 200) {
-            append_comment(xhr.response)
+            append_comment(xhr.response.comment)
             if (data.parent_id)
                 remove_comment_form(el)
         } else {
